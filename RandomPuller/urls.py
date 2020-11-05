3dc335f1947dcf47fa9520c0829ff006eae57a97
Alexander Mellor <AlexMellor2014@gmail.com>
2020-11-05 14:40:33 -0600

    Got preliminary output of pulled randoms

--- RandomPuller/urls.py
+++ RandomPuller/urls.py
@@ -7,5 +7,6 @@ app_name = 'RandomPuller'
 urlpatterns = [
     path('', views.index, name='index'),
     path('random_puller/', views.random_puller, name='random_puller'),
+    path('pulled_employees/', views.pulled_employees, name='pulled_employees'),
 ]
 
--- RandomPuller/views.py
+++ RandomPuller/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+
+import random
 
 from .models import Company, Employee
+
+SHUFFLE_NUMBER = 5
 
 
 # Create your views here.
@@ -30,3 +34,43 @@ def random_puller(request):
         'selected_company': selected_company,
     }
     return render(request, 'RandomPuller/random_puller.html', context)
+
+
+def pulled_employees(request):
+    if request.method == 'POST':
+        try:
+            rand_pull = int(request.POST.get('random_number_to_pull'))
+        except ValueError:
+            rand_pull = 0
+        try:
+            alt_pull = int(request.POST.get('alternate_number_to_pull'))
+        except ValueError:
+            alt_pull = 0
+        company_name = request.POST.get('company_name')
+    else:
+        rand_pull = 0
+        alt_pull = 0
+        company_name = None
+
+    employee_list = list(Employee.objects.filter(company_name__name=company_name))
+
+    if rand_pull + alt_pull >= len(employee_list):
+        return redirect('/random/')
+
+    for _ in range(SHUFFLE_NUMBER):
+        random.shuffle(employee_list)
+
+    random_employee_list = []
+    for pull in range(rand_pull):
+        random_employee_list.append(employee_list.pop(0))
+
+    alternate_employee_list = []
+    for pull in range(alt_pull):
+        alternate_employee_list.append(employee_list.pop(0))
+
+    context = {
+        'company': company_name,
+        'random_list': random_employee_list,
+        'alternate_list': alternate_employee_list,
+    }
+    return render(request, 'RandomPuller/pulled_employees.html', context)
