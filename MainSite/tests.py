52121f49d93359be14b0cfa5180ecfd2557b95da
Alexander Mellor <AlexMellor2014@gmail.com>
2020-11-18 17:15:42 -0600

    modified tests to use reverse instead of hard links

--- MainSite/tests.py
+++ MainSite/tests.py
@@ -1,29 +1,32 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your tests here.
 class TemplateTests(TestCase):
 
     def test_index(self):
-        response = self.client.get('/')
+        response = self.client.get(reverse('MainSite:index'))
         self.assertTemplateUsed(response, 'MainSite/index.html')
 
     def test_who_needs_a_consortium(self):
-        response = self.client.get('/who-needs-a-consortium/')
+        response = self.client.get(reverse('MainSite:who_needs_a_consortium'))
         self.assertTemplateUsed(response, 'MainSite/who_needs_a_consortium.html')
 
     def test_what_can_elite_do_for_you(self):
-        response = self.client.get('/what-can-elite-do-for-you/')
+        response = self.client.get(reverse('MainSite:what_can_elite_do_for_you'))
         self.assertTemplateUsed(response, 'MainSite/what_can_elite_do_for_you.html')
 
     def test_pricing(self):
-        response = self.client.get('/pricing/')
+        response = self.client.get(reverse('MainSite:pricing'))
         self.assertTemplateUsed(response, 'MainSite/pricing.html')
 
     def test_did_you_know_dot_requires(self):
-        response = self.client.get('/did-you-know-dot-requires/')
+        response = self.client.get(reverse('MainSite:did_you_know_dot_requires'))
         self.assertTemplateUsed(response, 'MainSite/did_you_know_dot_requires.html')
 
     def test_contact_us(self):
-        response = self.client.get('/contact-us/')
+        response = self.client.get(reverse('MainSite:contact_us'))
         self.assertTemplateUsed(response, 'MainSite/contact_us.html')
