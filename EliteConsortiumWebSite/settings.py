f53378939fb8756d93e997c81e55d3e8baaec22f
Alexander Mellor <AlexMellor2014@gmail.com>
2020-11-30 12:32:24 -0600

    Updated settings

--- EliteConsortiumWebSite/settings.py
+++ EliteConsortiumWebSite/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = environ.get('SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+]
 
 # Application definition
 
